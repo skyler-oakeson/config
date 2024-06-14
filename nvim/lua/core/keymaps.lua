c62a4e6e97b5d627927cc19dcc7f14d8fa445b3e
Soakeson <skyler.oakeson@gmail.com>
2024-06-14 15:53:03 -0600

    Fixed keymaps and tmux float size.

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -48,7 +48,10 @@ harpoon:setup()
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>b", function() harpoon.ui:toggle_quick_menu(harpoon:list(), {
   border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
-  title_pos = "center", ui_width_ratio = 0.40, })
+  title_pos = "center", 
+  ui_width_ratio = 0.40, 
+  title = " Harpoon "
+})
 end)
 vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
